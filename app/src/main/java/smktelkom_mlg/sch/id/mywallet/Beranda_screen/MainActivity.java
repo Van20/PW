e60b8b2274f393b330607d20d89af40f2abe2990
Rizvan <rizvanariyanto@gmail.com>
2018-01-07 09:05:23 +0700

    Welcome + Splash + Register + Login + Forgot

--- app/src/main/java/smktelkom_mlg/sch/id/mywallet/Beranda_screen/MainActivity.java
+++ app/src/main/java/smktelkom_mlg/sch/id/mywallet/Beranda_screen/MainActivity.java
@@ -1,6 +1,5 @@
 package smktelkom_mlg.sch.id.mywallet.Beranda_screen;
 
-import android.annotation.SuppressLint;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
@@ -8,25 +7,16 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.Color;
 import android.graphics.drawable.BitmapDrawable;
-import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
-import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.graphics.drawable.RoundedBitmapDrawable;
 import android.support.v4.graphics.drawable.RoundedBitmapDrawableFactory;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.AlertDialog;
-import android.text.Html;
-import android.text.Spannable;
-import android.text.SpannableString;
 import android.text.TextUtils;
-import android.text.style.ForegroundColorSpan;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.support.design.widget.NavigationView;
@@ -37,23 +27,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
-import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.github.mikephil.charting.utils.Utils;
-import com.google.android.gms.auth.api.Auth;
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-import com.google.android.gms.auth.api.signin.GoogleSignInResult;
-import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.common.api.OptionalPendingResult;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.UserInfo;
@@ -68,7 +47,6 @@ import smktelkom_mlg.sch.id.mywallet.Database_sql.Fragment.FragmentMonthlyReport
 import smktelkom_mlg.sch.id.mywallet.Database_sql.Fragment.FragmentPromo;
 import smktelkom_mlg.sch.id.mywallet.Database_sql.Utils.SPManager;
 import smktelkom_mlg.sch.id.mywallet.Login_screen.LoginActivity;
-import smktelkom_mlg.sch.id.mywallet.Promo_screen.PromoActivity;
 import smktelkom_mlg.sch.id.mywallet.R;
 
 public class MainActivity extends AppCompatActivity
